5866d6af88180fd46a9cba399d74179f9cf3edb7
Robson <53387930+HunterLowBit@users.noreply.github.com>
2023-09-02 20:41:37 -0300

    molde #5 volta do intervalo

--- main.js
+++ main.js
@@ -7,31 +7,33 @@
 // alert ("asd test");
 
 const catalago = [
-{
-  id: 1,
-  nome: "Casaco Branco",
-  marca: "Zara",
-  preco: 70,
-  nomeArquivoImagem: "product-1.jpg",
-},
-{
-  id: 2,
-  nome: "Sobretudo Azul",
-  marca: "Zara",
-  preco: 110,
-  nomeArquivoImagem: "product-2.jpg",
-}];
+  {
+    id: 1,
+    nome: "Casaco Branco",
+    marca: "Zara",
+    preco: 70,
+    nomeArquivoImagem: "product-1.jpg",
+  },
+  {
+    id: 2,
+    nome: "Sobretudo Azul",
+    marca: "Zara",
+    preco: 110,
+    nomeArquivoImagem: "product-2.jpg",
+  },
+];
 
-
-const cartaoProduto = `
+for (const produtoCatalago of catalago) {
+  const cartaoProduto = `
 <div id="card-produto-1">
 <img 
-    src="./assets/img/${catalago[0].nomeArquivoImagem}" alt="produto 1 HLB_Store" 
+    src="./assets/img/${produtoCatalago.nomeArquivoImagem}" alt="${produtoCatalago.nome}1 HLB_Store" 
     style="height:200px">
-    <p>${catalago[0].marca}</p>        
-    <p>${catalago[0].nome}</p>
-    <p>${catalago[0].preco}</p>
+    <p>${produtoCatalago.marca}</p>        
+    <p>${produtoCatalago.nome}</p>
+    <p>$${produtoCatalago.preco}</p>
     <button>Adicionar</button>
 </div>`;
 
-document.getElementById("container-produto").innerHTML += cartaoProduto;
+  document.getElementById("container-produto").innerHTML += cartaoProduto;
+};
