aa8e1a05cfff99220984fb24d3eb1152a91973ef
Robson <53387930+HunterLowBit@users.noreply.github.com>
2023-09-02 18:37:04 -0300

    molde #3

---
+++ main.js
@@ -0,0 +1,14 @@
+// const nomeProduto = "Casaco Branco";
+// const marca = "Zara";
+// const preco = 70;
+// const nomeArquivoImagem = "produto-1"
+// console.log(nomeProduto); // Casaco Branco
+// console.log(marca);
+// alert ("asd test");
+
+const produto1 = {
+  nome: "Casaco Branco",
+  marca: "Zara",
+  preco: 70,
+  nomeArquivoImagem: "product-1.jpg",
+};
