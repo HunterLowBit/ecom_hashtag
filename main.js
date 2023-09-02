34dec3692ca1efab75ab1e85d15064a0e8d536ba
Robson <53387930+HunterLowBit@users.noreply.github.com>
2023-09-02 19:08:55 -0300

    molde #4 JS
    Configurando os moldes no "main.js"

--- main.js
+++ main.js
@@ -6,9 +6,32 @@
 // console.log(marca);
 // alert ("asd test");
 
-const produto1 = {
+const catalago = [
+{
+  id: 1,
   nome: "Casaco Branco",
   marca: "Zara",
   preco: 70,
   nomeArquivoImagem: "product-1.jpg",
-};
+},
+{
+  id: 2,
+  nome: "Sobretudo Azul",
+  marca: "Zara",
+  preco: 110,
+  nomeArquivoImagem: "product-2.jpg",
+}];
+
+
+const cartaoProduto = `
+<div id="card-produto-1">
+<img 
+    src="./assets/img/${catalago[0].nomeArquivoImagem}" alt="produto 1 HLB_Store" 
+    style="height:200px">
+    <p>${catalago[0].marca}</p>        
+    <p>${catalago[0].nome}</p>
+    <p>${catalago[0].preco}</p>
+    <button>Adicionar</button>
+</div>`;
+
+document.getElementById("container-produto").innerHTML += cartaoProduto;
