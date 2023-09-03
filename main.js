8648bf96b53b839c445dd00e4e485a7fbaf4a782
Robson <53387930+HunterLowBit@users.noreply.github.com>
2023-09-02 21:30:12 -0300

    up

--- main.js
+++ main.js
@@ -21,11 +21,32 @@ const catalago = [
     preco: 110,
     nomeArquivoImagem: "product-2.jpg",
   },
+  {
+    id: 3,
+    nome: "Casaco Marrom",
+    marca: "Zara",
+    preco: 200,
+    nomeArquivoImagem: "product-3.jpg",
+  },
+  {
+    id: 4,
+    nome: "blazer",
+    marca: "Zara",
+    preco: 150,
+    nomeArquivoImagem: "product-4.jpg",
+  },
+  {
+    id: 5,
+    nome: "Jaqueta Jeans",
+    marca: "Zara",
+    preco: 110,
+    nomeArquivoImagem: "product-5.jpg",
+  },
 ];
 
 for (const produtoCatalago of catalago) {
   const cartaoProduto = `
-<div class="border-solid border-2 border-sky-500 w-48 my-2" id="card-produto-${produtoCatalago.id}">
+<div class="border-solid border-2 border-sky-500 w-48 m-2" id="card-produto-${produtoCatalago.id}">
 <img 
     src="./assets/img/${produtoCatalago.nomeArquivoImagem}" alt="${produtoCatalago.nome} HLB_Store" 
     style="height:300px">
