8ccc1400d6de1ad179bbeff2151db51fb7faac5c
Robson <53387930+HunterLowBit@users.noreply.github.com>
2023-09-03 00:09:23 -0300

    adicionar ao carrinho

--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -9,9 +9,25 @@ function fecharCarrinho() {
 }
 
 export function inicializarCarrinho() {
-    const botaoFecharCarrinho = document.getElementById("fechar-carrinho");
-    const botaoAbrirCarrinho = document.getElementById("abrir-carrinho");
+  const botaoFecharCarrinho = document.getElementById("fechar-carrinho");
+  const botaoAbrirCarrinho = document.getElementById("abrir-carrinho");
 
-    botaoFecharCarrinho.addEventListener("click", fecharCarrinho);
-    botaoAbrirCarrinho.addEventListener("click", abrirCarrinho);
+  botaoFecharCarrinho.addEventListener("click", fecharCarrinho);
+  botaoAbrirCarrinho.addEventListener("click", abrirCarrinho);
+}
+
+export function adicionarAoCarrinho() {
+    const containerProdutosCarrinho = document.getElementById('produtos-carrinho');
+  
+const cartaoProdutoCarrinho = `<article class="flex bg-purple-950 rounded-lg relative">
+        <button id="remover-produto"><i class=" absolute top-0 right-0 fa-solid fa-circle-xmark p-1 hover:text-red-700 hover:text-xl"></i></button>
+        <img src="./assets/img/product-1.jpg" alt="Blusa" class="h-24 rounded-lg p-1">
+        <div class="py-2 px-5">
+          <p class="text-gray-50 text-sm">Blusa </p>
+          <p class="text-gray-300 text-xs">Tamanho: M</p>
+          <p class="text-green-500 text-lg">$70 </p>
+        </div>
+      </article>`;
+
+      containerProdutosCarrinho.innerHTML += cartaoProdutoCarrinho;
 }
