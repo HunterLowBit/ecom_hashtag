c9ff556c1ab35bf34c4a3796949a63e5ff0db910
Robson <53387930+HunterLowBit@users.noreply.github.com>
2023-09-03 21:24:36 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/magazineIntensivao/",
+  base: "/ecom_hashtag/",
   plugins: [],
   resolve: {
     /*something*/
